c33ce1b08dfc6bb122f2be4574102318eaba83cb
Raphael <matosramosraphael@gmail.com>
2024-05-22 16:00:12 -0500

    codestream

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,9 @@
+require("newrelic");
+
+require("newrelic");
+
+require("newrelic");
+
 "use strict";
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
